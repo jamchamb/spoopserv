0703df0213c59c31840e739c90d1b430e3f91e52
James Chambers <jameschambers2@gmail.com>
2014-10-16 02:10:56 -0400

    Using GeoJSON for 2dsphere

--- add_ghost.php
+++ add_ghost.php
@@ -7,44 +7,56 @@ header('Content-Type: application/json');
 // Get ghosts collection
 $collection = $db->ghosts;
 
-if(!(isset($_POST['name']) && isset($_POST['user']) && isset($_POST['longitude']) && isset($_POST['latitude']) && isset($_POST['drawable']))) {
-    $result = array(
-		    "status" => "fail",
-		    "data" => array(
-				    "message" => "Not all fields were supplied"
-				    )
-		    );
-    echo json_encode($result);
-  } else {
+if(!
+   (isset($_POST['name']) &&
+    isset($_POST['user']) &&
+    isset($_POST['longitude']) &&
+    isset($_POST['latitude']) &&
+    isset($_POST['drawable'])
+    )
+   ) {
+  $result = 
+    array(
+	  "status" => "fail",
+	  "data" => array(
+			  "message" => "Not all fields were supplied"
+			  )
+	  );
 
+  echo json_encode($result);
+} else {
   $findme = array("name" => $_POST['name']);
   $cursor = $collection->find($findme);
+  
   if($cursor->count(true) > 0) {
-    $result = array("status" => "fail",
-		    "data" => array(
-				    "message" => "Name taken"
-				    )
-		    );
+    $result =
+      array("status" => "fail",
+	    "data" => array(
+			    "message" => "Name taken"
+			    )
+	    );
+
     echo json_encode($result);
-  } 
-  else {
-    $doc = array(
-	       "name" => $_POST['name'], 
-	       "user" => $_POST['user'],
-	       "drawable" => $_POST['drawable'],
-	       "location" => array(
-				   "longitude"=>$_POST['longitude'], 
-				   "latitude"=>$_POST['latitude']
-				   )
-	       );
+  } else {
+    $doc =
+      array(
+	    "name" => $_POST['name'], 
+	    "user" => $_POST['user'],
+	    "drawable" => $_POST['drawable'],
+	    "loc" => array(
+			   "type" => "Point",
+			   "coordinates" => array(floatval($_POST['longitude']), floatval($_POST['latitude']))
+		  )
+	    );
 
     $collection->insert($doc);
-  
-    $result = array("status" => "success",
-		    "data" => array(
-				    "message" => "Ghost added"
-				    )
-		    );
+
+    $result =
+      array("status" => "success",
+	    "data" => array(
+			    "message" => "Ghost added"
+			    )
+	    );
 
     echo json_encode($result);
   }
